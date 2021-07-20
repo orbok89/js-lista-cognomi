332c68e1c244a58fc0bf39d74985631132b3deda
BigFelk <fedferrara89@gmail.com>
2021-07-20 17:45:48 +0200

    modifica maiuscola automatica

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 var cognomi = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
 
 var  nuovo = prompt('cognome');
-cognomi.push(nuovo);
+var nuovo_1 = nuovo[0].toUpperCase() + nuovo.substr(1).toLowerCase();
+cognomi.push(nuovo_1);
+
 var cognomi_2 = cognomi.sort();
 console.log(cognomi_2);
 
-console.log(1 + cognomi_2.indexOf(nuovo));
+console.log(1 + cognomi_2.indexOf(nuovo_1));
+
+console.log(nuovo_1);
+
